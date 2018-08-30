67cff43f5c1ee71cb745ed1c285533c06660399d
Erician <zkliuchaojie@163.com>
2018-08-31 00:51:08 +0800

    break loop

--- logmanager/writelog.go
+++ logmanager/writelog.go
@@ -1,38 +1,51 @@
 package logmanager
 
 import (
+	"fmt"
 	"log"
 	"time"
 )
 
-//WriteLog write a LogRecord
-func WriteLog(lr *LogRecord) {
+//WriteLogRecord write a LogRecord
+func WriteLogRecord(lr *LogRecord) {
 	lr.re = make(chan struct{})
 	logManager.logRecordChan <- lr
 	<-lr.re
 }
 
-func (lm *LogManager) write() {
+func (lm *LogManager) writeLog() {
 	var res []chan struct{}
 	for {
 		lr := <-logManager.logRecordChan
 		lm.writeLogRecord(lr)
 		res = append(res, lr.re)
 		syncLogChan := time.After(time.Duration(LogManagerDefaultSyncLogInterval) * time.Millisecond)
+	loop:
 		for {
 			select {
 			case <-syncLogChan:
-				break
+				break loop
 			case lr = <-logManager.logRecordChan:
 				lm.writeLogRecord(lr)
 				res = append(res, lr.re)
 			}
+		}
+		if err := lm.syncLog(); err != nil {
+			//shutdown this system
+			log.Fatal("write log ", err)
 		}
 		for _, re := range res {
 			re <- struct{}{}
 		}
 		res = nil
 	}
+}
+
+func (lm *LogManager) syncLog() (err error) {
+	if err = lm.logFile.Sync(); err != nil {
+		return fmt.Errorf("sync log file, %v", err)
+	}
+	return
 }
 
 func (lm *LogManager) writeLogRecord(lr *LogRecord) {
